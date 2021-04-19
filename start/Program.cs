caa1fe021f49be339b32a07fa4ee3a45faa922b5
Neo_Deng <863494228@qq.com>
2021-04-19 10:38:01 +0800

    #1

---
+++ start/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace start
+{
+    class Program
+    {
+        //类的解释
+        static void Main(string[] args)
+        {
+            //打印hello world
+            Console.WriteLine("Hello World!");
+            
+            /*
+                C# 是大小写敏感的。
+                所有的语句和表达式必须以分号（;）结尾。
+                程序的执行从 Main 方法开始。
+            */
+            // 暂停，等待键盘响应
+            Console.ReadKey();
+        }
+    }
+}
